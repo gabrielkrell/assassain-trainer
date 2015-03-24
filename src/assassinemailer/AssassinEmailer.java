151392e1db815d47159477bbbae4c943e2a16109
gabrielkrell <gabrielkrell@gmail.com>
2015-03-23 20:38:12 -0500

    Initial structure of program. GmailSender - handles actual sending of emails AssassinEmailer.java -takes login info, email/name list, and message info -assigns a target to each person -uses GmailSender to send each person an email with relevant info

--- src/assassinemailer/AssassinEmailer.java
+++ src/assassinemailer/AssassinEmailer.java
@@ -1,9 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package assassinemailer;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
 /**
  *
@@ -11,11 +10,143 @@ package assassinemailer;
  */
 public class AssassinEmailer {
 
+    private ArrayList<String[]> emails; // {name, address}
+    private String userEmail;
+    private String userPassword;
+    private String messageText =  "Hi <recipient>!  You've been entered into a"
+            + " game of Assassin!  Your target is <target>.  The game will begin"
+            + " at <time> on <date>.";
+    private String time;
+    private String date;
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        /*
+        This project should:
+            Take an email/pass to send with (maybe initially gmail)
+            Take a list of emails and names to include in the game
+            Decide who's targeting who
+            Send everyone on the list an email telling them that they're playing,
+                who their target is, and the rules of the game.
+        */
+        System.out.println("Command-line operation is not yet supported!  Sorry!");
+        System.out.println("Right now, this method doubles as a tester.");
+        System.out.println("----------------------------------------------------");
+        
+        ArrayList<String[]> al = new ArrayList<>();
+        al.add(new String[] {"adam@gmail.com", "Adam"});
+        al.add(new String[] {"bob@cps.edu", "Bob"});
+        al.add(new String[] {"cathy@yahoo.com","Cathy"});
+        al.add(new String[] {"delilah@att.net","Delilah"});
+        
+        AssassinEmailer ae = new AssassinEmailer(al);
+        ae.setDate("DATE");
+        ae.setTime("TIME");
+        for (String s : ae.previewMessages()) {
+            System.out.println(s);
+            System.out.println();
+        }
+    }
+    
+    /** Create a new AssassinEmailer class.  Make sure to set the email, and
+     * password before using.  You can also run AssassinEmailer(ArrayList<String[]> emails)
+     * 
+     */
+    public AssassinEmailer() {
+        emails = new ArrayList<>();
+    }
+    
+    /** Create a new AssassiniEmailer() and set the email list to <code>emails</code>.
+     *
+     * @param emails
+     */
+    public AssassinEmailer(ArrayList<String[]> emails) {
+        this.emails = emails;
+    }
+    
+    public void setUserEmail( String uname) {
+        userEmail = uname;
+    }
+    
+    public void setUserPass ( String pword ) {
+        userPassword = pword;
+    }
+    
+    public String getMessage() {
+        return messageText;
+    }
+    
+    public void setMessage( String msg ) {
+        messageText = msg;
+    }
+    
+    public String getTime() {
+        return time;
+    }
+    
+    public void setTime( String t) {
+        time = t;
+    }
+    
+    public String getDate() {
+        return date;
+    }
+    
+    public void setDate(String d) {
+        date = d;
+    }
+    
+    public ArrayList<String> previewMessages() {
+        ArrayList<String> out = new ArrayList<>();
+        for (String[][] mailPair : generatePairs()) {
+            out.add(processMessage(mailPair[0][1], mailPair[1][1], time, date));
+        }
+        return out;
+    }
+    
+    private ArrayList<String[][]> generatePairs() {
+        // shuffle the email addresses and pair them so that there is only one loop.
+        // (each address leads to only one other and is lead to by only one other)
+        
+        ArrayList<String[]> newpairs = (ArrayList<String[]>) emails.clone();
+        Collections.shuffle(newpairs); // preserve original email order for editing later
+        
+        ArrayList<String[][]> output = new ArrayList<>();
+        for (int i=0; i<newpairs.size()-1; i++) {
+            output.add(new String[][]{newpairs.get(i),newpairs.get(i+1)});
+            System.out.println(Arrays.deepToString(output.get(output.size()-1)));
+        }
+        output.add(new String[][]{newpairs.get(newpairs.size()-1),newpairs.get(0)});
+        return output;
+    }
+    
+    private String processMessage(String recipient, String target, String time, String date) {
+        StringBuilder out = new StringBuilder(messageText);
+        replaceAll(out,"<recipient>",recipient);
+        replaceAll(out,"<target>",target);
+        replaceAll(out,"<time>",time);
+        replaceAll(out,"<date>",date);
+        return out.toString();
+    }
+    
+    /** Replace all instances of <code>find</code> with <code>replace</code> in
+     * the specified StringBuilder.
+     * 
+     * @param input StringBuilder to modify
+     * @param find String to remove occurrences of
+     * @param replace String to replace <code>find</code>s with
+     */
+    private void replaceAll (StringBuilder input, String find, String replace) {
+//        StringBuilder sb = new StringBuilder(input);
+        StringBuilder sb = input;
+        while (sb.indexOf(find)>0) {
+            sb = sb.replace(sb.indexOf(find), sb.indexOf(find)+find.length(), replace);
+//            System.out.println(sb);
+//            input = input.substring(0,input.indexOf(find)-1)+input.substring(input.indexOf(find)+find.length()+2);
+        }
         
     }
+    
     
 }
---
+++ src/assassinemailer/GmailSender.java
@@ -0,0 +1,119 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2015 Gabriel Krell.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package assassinemailer;
+
+import com.sun.mail.smtp.SMTPTransport;
+import java.security.Security;
+import java.util.Date;
+import java.util.Properties;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+/** Modified code from an answer on Stack Exchange.
+ *
+ * @author doraemon
+ * @author Gabriel Krell
+ */
+public class GmailSender {
+    private GmailSender() {
+    }
+
+    /**
+     * Send email using GMail SMTP server.
+     *
+     * @param username GMail username
+     * @param password GMail password
+     * @param recipientEmail TO recipient
+     * @param title title of the message
+     * @param message message to be sent
+     * @throws AddressException if the email address parse failed
+     * @throws MessagingException if the connection is dead or not in the connected state or if the message is not a MimeMessage
+     */
+    public static void Send(final String username, final String password, String recipientEmail, String title, String message) throws AddressException, MessagingException {
+        GmailSender.Send(username, password, recipientEmail, "", title, message);
+    }
+
+    /**
+     * Send email using GMail SMTP server.
+     *
+     * @param username GMail username
+     * @param password GMail password
+     * @param recipientEmail TO recipient
+     * @param ccEmail CC recipient. Can be empty if there is no CC recipient
+     * @param title title of the message
+     * @param message message to be sent
+     * @throws AddressException if the email address parse failed
+     * @throws MessagingException if the connection is dead or not in the connected state or if the message is not a MimeMessage
+     */
+    public static void Send(final String username, final String password, String recipientEmail, String ccEmail, String title, String message) throws AddressException, MessagingException {
+        Security.addProvider(new com.sun.net.ssl.internal.ssl.Provider());
+        final String SSL_FACTORY = "javax.net.ssl.SSLSocketFactory";
+
+        // Get a Properties object
+        Properties props = System.getProperties();
+        props.setProperty("mail.smtps.host", "smtp.gmail.com");
+        props.setProperty("mail.smtp.socketFactory.class", SSL_FACTORY);
+        props.setProperty("mail.smtp.socketFactory.fallback", "false");
+        props.setProperty("mail.smtp.port", "465");
+        props.setProperty("mail.smtp.socketFactory.port", "465");
+        props.setProperty("mail.smtps.auth", "true");
+
+        /*
+        If set to false, the QUIT command is sent and the connection is immediately closed. If set 
+        to true (the default), causes the transport to wait for the response to the QUIT command.
+
+        ref :   http://java.sun.com/products/javamail/javadocs/com/sun/mail/smtp/package-summary.html
+                http://forum.java.sun.com/thread.jspa?threadID=5205249
+                smtpsend.java - demo program from javamail
+        */
+        props.put("mail.smtps.quitwait", "false");
+
+        Session session = Session.getInstance(props, null);
+
+        // -- Create a new message --
+        final MimeMessage msg = new MimeMessage(session);
+
+        // -- Set the FROM and TO fields --
+        msg.setFrom(new InternetAddress(username + "@gmail.com"));
+        msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(recipientEmail, false));
+
+        if (ccEmail.length() > 0) {
+            msg.setRecipients(Message.RecipientType.CC, InternetAddress.parse(ccEmail, false));
+        }
+
+        msg.setSubject(title);
+        msg.setText(message, "utf-8");
+        msg.setSentDate(new Date());
+
+        SMTPTransport t = (SMTPTransport)session.getTransport("smtps");
+
+        t.connect("smtp.gmail.com", username, password);
+        t.sendMessage(msg, msg.getAllRecipients());      
+        t.close();
+    }
+}
